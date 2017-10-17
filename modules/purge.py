ebea56243968790334c5726fa9c5c85de0232532
Phaser Array <phaserarrayksp@gmail.com>
2017-10-17 23:45:59 +0300

    Reduced auto-remove bot msgs to 5 seconds, improved debug print

--- modules/purge.py
+++ modules/purge.py
@@ -18,6 +18,14 @@ class Purge:
 	@commands.command(pass_context=True)
 	async def purge(self, ctx, count=None, param=None):
 
+		self.logger.debug(
+			"Purging! Channel: {0}, Count: {1}, Param: {2}".format(
+				ctx.message.channel.id,
+				count,
+				param
+			)
+		)
+
 		await self.bot.delete_message(ctx.message)
 
 		_count = 0
@@ -25,15 +33,15 @@ class Purge:
 			_count = int(count)
 		except ValueError:
 			await self.bot.say(content="❌ **Could not parse count!**",
-			delete_after=10)
+			delete_after=5)
 			return
 		except TypeError:
 			await self.bot.say(content="❌ **Count not provided!**",
-			delete_after=10)
+			delete_after=5)
 			return
 		if _count < 1:
 			await self.bot.say(content="❌ **Count must be at least one!**",
-			delete_after=10)
+			delete_after=5)
 			return
 
 		permitted = False
@@ -44,18 +52,17 @@ class Purge:
 				permitted = True
 		if not permitted:
 			await self.bot.say(content="❌ **You are not allowed to use this!**",
-			delete_after=10)
+			delete_after=5)
 			return
 
 		member = None
 		if param is not None:
 			if param.startswith("<@") and not param.startswith("<@&"):
 				user_id = param.strip("<").strip(">").strip("@").strip("!")
-				self.logger.debug(user_id)
 				member = ctx.message.server.get_member(user_id)
 				if member is None:
 					await self.bot.say(content="❌ **Member not found!**",
-					delete_after=10)
+					delete_after=5)
 					return
 
 		def check_func(msg):
@@ -74,4 +81,4 @@ class Purge:
 			check=check_func)
 
 		await self.bot.say(content="✅ **Messages purged!**",
-			delete_after=10)
+			delete_after=5)
